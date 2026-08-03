2979108e979ee7fb975619e4c633878f1a1ebbd3
UPPUNUTULA HARSHITH <uppunutulaharshith@gmail.com>
2026-08-03 09:57:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import os 
+import gradio as gr
+from google import genai
+from google import genai
+from google.genai import types
+
+api_key =os.getenv('GEMINI_API')
+client = genai.Client(api_key=api_key)
+personalities = {
+  "Friendly":
+  "You are a friendly, enthusiastic, and highly encouraging Study Assistant. Your goal is to break down complex concepts into simple, beginner-friendly explanations. Use analogies and real-world examples that beginners can relate to. Always ask a follow-up question to check understanding",
+  "Academic":
+  "You are a strictly academic, highly detailed, and professional university Professor. Use precise, formal terminology, cite key concepts and structure your response. Your goal is to break down complex concepts into simple, beginner-friendly explanations. Use analogies and real-world examples that beginners can relate to. Always ask a follow-up question to check understanding"
+}
+def study_assistant(question, persona):
+    system_prompt = personalities[persona]
+
+    response = client.models.generate_content(
+        model="gemini-3.5-flash",
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt,
+            temperature=0.4,
+            max_output_tokens=1000
+        ),
+        contents=question
+    )
+    return response
+  demo = gr.Interface(
+    fn=study_assistant,
+    inputs=[
+        gr.Textbox(lines=4, placeholder="Ask a question...", label="Question"),
+        gr.Radio(choices=list(personalities.keys()), value="Friendly", label="Personality")
+    ],
+    outputs=gr.Textbox(lines=10, label="Response"),
+    title="Study Assistant",
+    description="Ask a question and get an answer from your AI study assistant with a chosen personality."
+)
+demo.launch(server_name="0.0.0.0",
+    server_port=int(os.enciron.get("PORT", 7860)))
